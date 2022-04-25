ab1bffe2e04fb996df37590ca9be0be460c24f3a
Christina_Graham <cgraham74@gmail.com>
2022-04-24 22:24:02 -0500

    Created a Linked List Class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args){
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.removeLast();
+        System.out.println(list.indexOf(10));
+        System.out.println(list.contains(40));
+        System.out.println(list.contains(10));
+
+    }
+}
